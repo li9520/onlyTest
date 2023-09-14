ce1607f98d19710f0cb0126564b7622cfe8a4e7a
li9520 <li.dp9520@gmail.com>
2023-09-14 15:48:50 +0500

    init

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+import { createRoot } from 'react-dom/client';
+
+import App from './App';
+
+const root = createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <App />
+);
+
+if (module.hot) {
+  module.hot.accept();
+}
